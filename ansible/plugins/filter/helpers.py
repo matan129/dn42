d3cedbb6f94edb333e1bd866f2b9712ba4cb249d
Matan Rosenberg <matan.rosenberg@forter.com>
2022-05-08 01:40:31 +0300

    Refactor looking glass

--- ansible/plugins/filter/helpers.py
+++ ansible/plugins/filter/helpers.py
@@ -1,10 +1,17 @@
+from typing import List, Dict, Any, Mapping, Union
 
-def format(items, pattern):
+
+def _format(items: Union[List, Dict], pattern: str) -> str:
+    if isinstance(items, dict):
+        return pattern.format(**items)
+
     return pattern.format(*items)
+
+
+def multi_extract(key: str, container: Mapping[str, Any], subkeys: List[str]) -> List[Any]:
+    return [container[key][subkey] for subkey in subkeys]
 
 
 class FilterModule:
     def filters(self):
-        return {
-            'format': format
-        }
+        return {"format": _format, "multi_extract": multi_extract}
